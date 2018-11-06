d8ba9239273219b366c0b3289c020ffd8379ff81
DanHutch <danhutch5280@gmail.com>
2018-11-05 17:29:55 -0700

    wrote some more tests

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -2,6 +2,7 @@ class Coupon < ApplicationRecord
 	validates :code, presence: true, uniqueness: true
 	validates :coupon_value, presence:true, numericality: true
 	validates :min_order, presence: true, numericality: true
+	validates :min_order, :numericality => {greater_than_or_equal_to: 0}
 	validates :variety, presence: true
 
 	enum variety: %w(percent amount)
--- spec/models/coupon_spec.rb
+++ spec/models/coupon_spec.rb
@@ -22,6 +22,24 @@ RSpec.describe Coupon, type: :model do
 		expect(coupon.variety).to eq("amount")
 		expect(Coupon.all.size).to eq(1)
 	end
+	it "should successfully create a coupon even without giving it a minimum order amount" do
+		expect(Coupon.all.size).to eq(0)
+		code = SecureRandom.uuid
+		coupon = Coupon.create(code: code, coupon_value: 10, variety: 1)
+
+		expect(coupon.code).to eq(code)
+		expect(coupon.coupon_value).to eq(10)
+		expect(coupon.min_order).to eq(0)
+		expect(coupon.variety).to eq("amount")
+		expect(Coupon.all.size).to eq(1)
+	end
+	it "should not create a coupon if the min order amount is less than zero" do
+		expect(Coupon.all.size).to eq(0)
+		code = SecureRandom.uuid
+		coupon = Coupon.create(code: code, coupon_value: 10, min_order: -5, variety: 1)
+
+		expect(Coupon.all.size).to eq(0)
+	end
 
 	# it "should not create a coupon with a value greater than the minimum order" do
 	# 	expect(Coupon.all).to eq([])
